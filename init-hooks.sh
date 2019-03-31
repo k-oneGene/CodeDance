39e72905171bedece93248693314c2c56ff541ae
Jin Park <jinpark.han@gmail.com>
2019-03-31 12:22:57 +0100

    Add hook file for Jenkins and git logline. Add requirement for deploayment

--- init-hooks.sh
+++ init-hooks.sh
@@ -3,3 +3,6 @@ set -e
 
 git config core.hooksPath dev/hooks
 chmod +x dev/hooks/pre-push
+
+# Make pretty git log "git loglin"
+git config --global alias.logline "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
